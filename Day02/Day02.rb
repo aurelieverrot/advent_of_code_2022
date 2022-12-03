a66c0babf41e95b272ff00857c4303e30190eadd
Aurelie Verrot <aurelie.verrot@zendesk.com>
2022-12-02 16:34:25 -0800

    day 2, part 1

--- Day02/Day02.rb
+++ Day02/Day02.rb
@@ -2,20 +2,31 @@ file = File.open("Day02/input2.txt")
 data = file.readlines.map(&:chomp)
 file.close
 
+wins = {
+  values: ["A Y", "C X", "B Z"],
+  points: 6
+}
 
-# example data
-# A Y
-# B Y
-# B Z
-# B Z
-# B X
-# B Z
-# C Y
+draws = {
+  values: ["A X", "B Y", "C Z"],
+  points: 3
+}
 
-# A Rock     X   1
-# B Paper    Y   2
-# C Scissors Z   3
+shapes = {
+  "X": 1,
+  "Y": 2,
+  "Z": 3
+}
+# Part 1
+my_points = 0
 
-# paper wins rock
-# rock wins scissors
-# scissors wins paper
+data.each do |line|
+  my_points += shapes.fetch(:"#{line[2]}")
+  if wins[:values].include?(line)
+    my_points += wins[:points]
+  elsif draws[:values].include?(line)
+    my_points += draws[:points]
+  end
+  my_points
+end
+
