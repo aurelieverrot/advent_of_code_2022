ad9251be72600f157976e3d7d73c733658955532
aurelieverrot <aurelie+github@verrot.fr>
2022-12-17 23:30:11 -0800

    part 2

--- Day03/Day03.rb
+++ Day03/Day03.rb
@@ -1,5 +1,4 @@
 file = File.open("Day03/input3.txt")
-# file = File.open("Day03/small_input.txt")
 data = file.readlines.map(&:chomp)
 file.close
 
@@ -13,9 +12,6 @@ data.each do |line|
   compartments = [line[0...line_cut].split(''), line[line_cut..-1].split('')]
 
   items << (compartments[0] & compartments[1]).first
-  # compartments[0].each_char do |letter|
-  #   items << letter if compartments[1].include?(letter)
-  # end
 
   items
 end
@@ -23,4 +19,20 @@ end
 items.each do |letter|
   priority_sum += priority_list.index(letter) + 1
 end
-puts priority_sum
+# puts priority_sum
+
+
+### part 2
+new_items = []
+new_priority_sum = 0
+
+data.each_slice(3).to_a.inject(0) do |sum,group|
+  elf1,elf2,elf3 = group.map { |elf| elf.split("") }
+  shared = (elf1 & elf2 & elf3).first
+  new_items << shared
+end
+
+new_items.each do |letter|
+  new_priority_sum += priority_list.index(letter) + 1
+end
+# puts new_priority_sum
