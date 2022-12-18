269400810510b4df9868003eccad84586acc84b5
aurelieverrot <aurelie+github@verrot.fr>
2022-12-18 12:45:51 -0800

    part 1

--- Day04/Day04.rb
+++ Day04/Day04.rb
@@ -2,3 +2,16 @@ file = File.open("Day04/input4.txt")
 data = file.readlines.map(&:chomp)
 file.close
 
+### part 1
+sum = 0
+data.each do |line|
+  elf1, elf2 = line.split(",")
+  elf1_ids = elf1.split("-").map(&:to_i)
+  elf2_ids = elf2.split("-").map(&:to_i)
+
+  if ((elf1_ids[0] >= elf2_ids[0]) & (elf1_ids[1] <= elf2_ids[1])) || ((elf2_ids[0] >= elf1_ids[0]) & (elf2_ids[1] <= elf1_ids[1]))
+    sum += 1
+  end
+  sum
+end
+puts sum
