97a04eda0ebd1dd9b8310c7386c998abd80bf757
aurelieverrot <aurelie+github@verrot.fr>
2022-12-18 20:09:12 -0800

    part 2

--- Day04/Day04.rb
+++ Day04/Day04.rb
@@ -15,3 +15,17 @@ data.each do |line|
   sum
 end
 puts sum
+
+### part 2
+sum_2 = 0
+data.each do |line|
+  elf1, elf2 = line.split(",")
+  elf1_ids = elf1.split("-").map(&:to_i)
+  elf2_ids = elf2.split("-").map(&:to_i)
+
+  if (elf1_ids[1] >= elf2_ids[0]) && (elf1_ids[0] <= elf2_ids[1])
+    sum_2 += 1
+  end
+  sum_2
+end
+puts sum_2
